0a0ae4eac15fc25822785ed633492f93c0a7ae34
kaushalluffa <webdevelopment852@gmail.com>
2022-07-24 21:55:47 -0400

    check answer function added now tells correct or wrong answer

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -2,7 +2,7 @@ const categoryEl = document.getElementById("category");
 const difficultyEl = document.getElementById("difficulty");
 const questionEl = document.getElementById("question");
 const ans = document.querySelector(".container__answers");
-const checkAnswer = document.getElementById('check')
+const checkAnsBtn = document.getElementById("check");
 axios
   .get("https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple")
   .then((res) => {
@@ -21,15 +21,57 @@ axios
     const shuffledArray = shuffleArray(arr);
 
     for (let i = 0; i < arr.length; i++) {
-      
       const option = document.createElement("div");
-      option.setAttribute("id", `option${i +1}`);
+      const check = document.createElement("input");
+      check.setAttribute("type", "checkbox");
+      option.setAttribute("id", `option${i + 1}`);
       option.classList.add("container__answer");
-      option.innerHTML = arr[i];
+
+      option.innerHTML = `<input type="radio" name='option' id='option' value='${arr[i]}' /> <span> ${arr[i]}</span>`;
 
       ans.appendChild(option);
     }
     categoryEl.innerText = category;
     difficultyEl.innerText = `Difficulty Level: ${difficulty}`;
     questionEl.innerHTML = question;
+
+    checkAnsBtn.addEventListener("click", checkAnswer);
+    function checkAnswer() {
+      const container = document.getElementById("container");
+      const checked = document.querySelector(`input[name='option']:checked`);
+      if (
+        checked &&
+        checked.value === `${res.data.results[0].correct_answer}`
+      ) {
+        const correct = document.createElement('p')
+        correct.classList.add('correct')
+        correct.innerText = `You are Correct. Congratulations`
+        ans.innerHTML = ''
+        ans.appendChild(correct)
+        const nextBtn = document.createElement("button");
+        nextBtn.classList.add("next");
+        nextBtn.innerText = "Next";
+        container.appendChild(nextBtn);
+        nextBtn.addEventListener("click", () => {
+          location.reload();
+        });
+      } else {
+        ans.innerHTML = "";
+        const correctAnswer = document.createElement("p");
+        correctAnswer.classList.add("correct");
+        correctAnswer.innerText = `This is the correct answer ${res.data.results[0].correct_answer}`;
+        const wrongAnswer = document.createElement("p");
+        wrongAnswer.classList.add("wrong");
+        wrongAnswer.innerText = "Sorry Wrong Answer";
+        ans.appendChild(wrongAnswer);
+        ans.appendChild(correctAnswer);
+        const nextBtn = document.createElement("button");
+        nextBtn.classList.add("next");
+        nextBtn.innerText = "Next";
+        container.appendChild(nextBtn);
+        nextBtn.addEventListener("click", () => {
+          location.reload();
+        });
+      }
+    }
   });
