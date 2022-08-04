584c653a677d93e8c25107a5502d87d1ceea29ff
kaushalluffa <webdevelopment852@gmail.com>
2022-08-04 09:08:03 -0400

    implemented routes in gh-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
 import Quiz from "./components/Quiz";
 
